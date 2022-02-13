e75dde701cc51663cdc06b1c6f06760d4c33629b
Dayoung <dy.kim@datadriven.kr>
2022-02-14 01:30:58 +0900

    button story 생성(작업 전)

---
+++ src/components/atoms/Button/index.stories.tsx
@@ -0,0 +1,10 @@
+import ToggleButton from '../ToggleButton';
+
+export default {
+  title: 'atoms/Button',
+  component: ToggleButton,
+};
+
+export const Sample = () => {
+  return <ToggleButton />;
+};
